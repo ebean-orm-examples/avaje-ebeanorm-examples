d53ca5ca4ec50d21c5881dec23f7c959e157cc2b
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-02 14:57:46 +1200

    CallableSqlTest with an IN and OUT parameter

---
+++ x-postgres-features/src/test/java/org/example/storedprocedure/CallableSqlTest.java
@@ -0,0 +1,32 @@
+package org.example.storedprocedure;
+
+import com.avaje.ebean.CallableSql;
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import org.example.ExampleBaseTestCase;
+import org.junit.Test;
+
+import java.sql.Types;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CallableSqlTest extends ExampleBaseTestCase {
+
+  @Test
+  public void testCallable() {
+
+    EbeanServer server = Ebean.getDefaultServer();
+
+    //create or replace function sp_insert (p_name VARCHAR, OUT p_id INTEGER)
+    CallableSql callableSql = server.createCallableSql("{call sp_insert(?, ?)}");
+    callableSql.setParameter(1, "rob name");
+    callableSql.registerOut(2, Types.INTEGER);
+
+    server.execute(callableSql);
+
+    Integer value = (Integer)callableSql.getObject(2);
+
+    assertThat(value).isNotNull();
+  }
+}
---
+++ x-postgres-features/src/test/resources/createStoredProcedure.sql
@@ -0,0 +1,15 @@
+create or replace function sp_insert (p_name VARCHAR, OUT p_id INTEGER)
+as $$
+
+DECLARE
+
+BEGIN
+  insert into p_customer (name, inactive, version, when_created, when_updated) values (p_name, true, 1, current_timestamp, current_timestamp);
+  SELECT currval(pg_get_serial_sequence('p_customer','id')) into p_id;
+END;
+$$
+LANGUAGE 'plpgsql'
+
+--  select sp_insert('asd');
+
+select * from p_customer ORDER BY id desc;
