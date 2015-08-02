524f652511fac783976905b22c3e6acf8e5bd446
Robin Bygrave <robin.bygrave@gmail.com>
2015-08-02 15:22:48 +1200

    CallableSqlTest with an INOUT parameter (test for https://github.com/ebean-orm/avaje-ebeanorm/issues/364)

--- x-postgres-features/src/test/java/org/example/storedprocedure/CallableSqlTest.java
+++ x-postgres-features/src/test/java/org/example/storedprocedure/CallableSqlTest.java
@@ -29,4 +29,25 @@ public class CallableSqlTest extends ExampleBaseTestCase {
 
     assertThat(value).isNotNull();
   }
+
+
+  @Test
+  public void testCallable_withInOut() {
+
+    EbeanServer server = Ebean.getDefaultServer();
+
+    //create or replace function sp_insert (p_name VARCHAR, OUT p_id INTEGER)
+    CallableSql callableSql = server.createCallableSql("{call sp_insert2(?, ?)}");
+    callableSql.setParameter(1, "rob name");
+    callableSql.registerOut(1, Types.VARCHAR);
+    callableSql.registerOut(2, Types.INTEGER);
+
+    server.execute(callableSql);
+
+    String nameOut = (String)callableSql.getObject(1);
+    Integer intOut = (Integer)callableSql.getObject(2);
+
+    assertThat(nameOut).isNotNull();
+    assertThat(intOut).isNotNull();
+  }
 }
--- x-postgres-features/src/test/resources/createStoredProcedure.sql
+++ x-postgres-features/src/test/resources/createStoredProcedure.sql
@@ -13,3 +13,14 @@ LANGUAGE 'plpgsql'
 --  select sp_insert('asd');
 
 select * from p_customer ORDER BY id desc;
+
+
+
+CREATE OR REPLACE FUNCTION sp_insert2 (INOUT p_name VARCHAR, OUT p_id INTEGER)
+AS $$
+BEGIN
+  insert into p_customer (name, inactive, version, when_created, when_updated) values (p_name, true, 1, current_timestamp, current_timestamp);
+  SELECT currval(pg_get_serial_sequence('p_customer','id')) into p_id;
+END;
+$$
+LANGUAGE 'plpgsql'
