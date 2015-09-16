a6894e79fd57f3db96d2cf909808aaabb2ff1cd4
Robin Bygrave <robin.bygrave@gmail.com>
2015-09-16 16:48:58 +1200

    Rename to MainQueryBeanGenerator

--- a-basic/src/test/java/main/MainQueryBeanGenerator.java
+++ a-basic/src/test/java/main/MainQueryBeanGenerator.java
@@ -8,27 +8,26 @@ import java.io.IOException;
 /**
  * Generate type query beans for each entity bean.
  */
-public class MainTypeBeanGenerator {
+public class MainQueryBeanGenerator {
 
   public static void main(String[] args) throws IOException {
 
-
     GeneratorConfig config = new GeneratorConfig();
-    //config.setClassesDirectory("./target/test-classes");
-    //config.setDestDirectory("./src/test/java");
-    //config.setDestResourceDirectory("./src/test/resources");
+    //config.setClassesDirectory("./target/classes");
+    //config.setDestDirectory("./src/main/java");
+    //config.setDestResourceDirectory("./src/main/resources");
 
     config.setEntityBeanPackage("org.example.domain");
     //config.setDestPackage("org.example.domain.query");
     //config.setMaxPathTraversalDepth(3);
     //config.setAopStyle(false);
 
-    config.setOverwriteExistingFinders(true);
+    //config.setOverwriteExistingFinders(true);
 
     Generator generator = new Generator(config);
     generator.generateQueryBeans();
-    generator.generateFinders();
-    generator.modifyEntityBeansAddFinderField();
+    //generator.generateFinders();
+    //generator.modifyEntityBeansAddFinderField();
 
   }
 }
